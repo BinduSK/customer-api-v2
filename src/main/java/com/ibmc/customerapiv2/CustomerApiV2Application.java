7221e9cbf166e025e35b0092abccff5934da6fba
BinduSK <bindusajithkumar01@gmail.com>
2022-08-25 06:09:26 +0530

    first commit

---
+++ src/main/java/com/ibmc/customerapiv2/CustomerApiV2Application.java
@@ -0,0 +1,13 @@
+package com.ibmc.customerapiv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerApiV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerApiV2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/ibmc/customerapiv2/CustomerApiV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ibmc.customerapiv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerApiV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
